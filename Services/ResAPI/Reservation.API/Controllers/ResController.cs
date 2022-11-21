efe8e8f581a3887e57ae464e9a8ac51dbe7f0039
engineer-kadir <kadiryildizmeb@gmail.com>
2022-11-21 22:44:07 +0300

    first commit

---
+++ Services/ResAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+
+        public ResController(IReservationService ReservationService)
+        {
+          reservationService = ReservationService;
+        }
+
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int Id)
+        {
+            return reservationService.GetResByBkgNumber(Id); 
+        }
+    }
+}
